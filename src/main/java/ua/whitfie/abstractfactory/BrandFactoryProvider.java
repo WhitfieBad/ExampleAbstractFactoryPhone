7d5e47d5cb41afbe04da1439d82f2297a4909773
whitfie <coreyoungnight@gmail.com>
2021-05-01 22:16:02 +0300

    fix

--- src/main/java/ua/whitfie/abstractfactory/BrandFactoryProvider.java
+++ src/main/java/ua/whitfie/abstractfactory/BrandFactoryProvider.java
@@ -4,7 +4,6 @@ public class BrandFactoryProvider {
 
     public static ProductBrandFactory getFactoryOfBrandName(String nameBrand) {
         switch (nameBrand.toLowerCase()) {
-            case "bmw" : return new BmwFactory();
             case "china" : return new ChinaFactory();
             case "apple" : return new AppleFactory();
             case "samsung" : return new SamsungFactory();
--- src/main/java/ua/whitfie/model/BmwProduct.java
+++
@@ -1,7 +0,0 @@
-package ua.whitfie.model;
-
-public class BmwProduct extends AbstractProduct {
-    public BmwProduct(String nameModel, String serialNumber) {
-        super("BMW", nameModel, serialNumber);
-    }
-}
